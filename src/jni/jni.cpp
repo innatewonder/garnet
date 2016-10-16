d071ff8632e85a4fae50cfafdc0e5d9b0463deff
Harrison <harrisonbeachey@gmail.com>
2016-10-16 14:40:28 -0400

    adding input handlers

--- src/jni/jni.cpp
+++ src/jni/jni.cpp
@@ -3,9 +3,15 @@
 
 void on_surface_created(JNIEnv* env, jclass cls)
 {
-  ArgParser parser(0, nullptr);
-  Run(parser);
-  Graphics::Create();
+  static bool created = false;
+
+  if(!created) 
+  {
+    ArgParser parser(0, nullptr);
+    Run(parser);
+    Graphics::Create();
+    created = true;
+  }
 }
 
 void on_surface_changed(JNIEnv* env, jclass cls, jint width, jint height)
@@ -23,10 +29,39 @@ void on_draw_frame(JNIEnv* env, jclass cls)
   Graphics::Frame();
 }
 
+void handleTouchPress(JNIEnv* env, jclass cls, jfloat x, jfloat y)
+{
+  Message* m = MESSAGE(Message::MSG_MOUSE_BUTTON);
+  m->AddData(DAT(x));
+  m->AddData(DAT(y));
+  m->AddData(true);
+  Core::gCore->SendMessage(m);
+}
+
+void handleTouchRelease(JNIEnv* env, jclass cls, jfloat x, jfloat y)
+{
+  Message* m = MESSAGE(Message::MSG_MOUSE_BUTTON);
+  m->AddData(DAT(x));
+  m->AddData(DAT(y));
+  m->AddData(false);
+  Core::gCore->SendMessage(m);
+}
+
+void handleTouchDrag(JNIEnv* env, jclass cls, jfloat x, jfloat y)
+{
+  Message* m = MESSAGE(Message::MSG_MOUSE);
+  m->AddData(DAT(x));
+  m->AddData(DAT(y));
+  Core::gCore->SendMessage(m);
+}
+
 static JNINativeMethod methodTable[] = {
   {"on_surface_created", "()V", (void *) on_surface_created},
   {"on_surface_changed", "(II)V", (void *) on_surface_changed},
   {"on_draw_frame", "()V", (void *) on_draw_frame},
+  {"handleTouchPress", "(FF)V", (void *) handleTouchPress},
+  {"handleTouchRelease", "(FF)V", (void *) handleTouchRelease},
+  {"handleTouchDrag", "(FF)V", (void *) handleTouchDrag},
 };
 
 static JNIEnv* env;
